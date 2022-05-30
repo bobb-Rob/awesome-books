763575319da277a67eeee7a8ade2f5c4e3c898dd
Robertson Akpan <robertsonakpan@gmail.com>
2022-05-30 14:49:37 +0100

    initial file set up

---
+++ index.js
@@ -0,0 +1,45 @@
+import Book from "./modules/book.js";
+import { getBooks, addBookToLS, removeBookLS } from "./modules/localStorage.js";
+import BookCollection from "./modules/bookCollection.js";
+import handleLinkClick from "./modules/navbarLinks.js";
+import clearFormFields from "./modules/clearForm.js";
+
+// Display books on page load
+window.addEventListener('DOMContentLoaded', BookCollection.bookDisplay);
+
+// Add Book Event
+const formEl = document.querySelector('#book-form');
+formEl.addEventListener('click', (e) => {
+  e.preventDefault();
+  const title = document.querySelector('#book-name').value;
+  const author = document.querySelector('#book-author').value;
+
+  if (title !== '' && author !== '') {
+    if (e.target.classList.contains('addButton')) {
+      const newBook = new Book(title, author);
+      newBook.id = BookCollection.bookList.length + 1;
+      BookCollection.addBookUI(newBook);
+      addBookToLS(newBook); // Add book to booklist in localStorage
+      clearFormFields();
+      formEl.submit();
+    }
+  }
+});
+
+// Remove Book from UI, localStorage and arrayList
+const bookContainer = document.querySelector('.booksContainer');
+bookContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove-btn')) {
+    const bookId = e.target.parentElement.id; // Card holder ID traversed here
+    BookCollection.removeBook(bookId);
+    removeBookLS(bookId);
+    e.target.parentElement.remove();
+  }
+});
+
+// Nav-links event
+document.getElementById('nav-links').addEventListener('click', handleLinkClick);
+document.getElementById('defaultOpen').click(); // Click on the list nav-link at default on page load
+
+
+
